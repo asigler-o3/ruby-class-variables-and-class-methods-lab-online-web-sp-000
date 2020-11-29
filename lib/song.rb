776956df68698b77bf9e0e6485a68015c77db0f8
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-29 07:58:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,14 @@ class Song
   end
 
   def self.genre_count
-
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 end
